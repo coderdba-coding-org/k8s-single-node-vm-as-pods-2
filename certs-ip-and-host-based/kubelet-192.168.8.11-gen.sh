99e1c8e4709078c2847b22a78452d0223b051263
coderdba <coderdba@coder.com>
2019-09-06 15:10:35 +0530

    update

--- certs-ip-and-host-based/kubelet-192.168.8.11-gen.sh
+++ certs-ip-and-host-based/kubelet-192.168.8.11-gen.sh
@@ -4,4 +4,4 @@ cfssl gencert \
 -config=ca-config.json \
 -hostname=192.168.8.11,ksn1 \
 -profile=kubernetes kubelet-192.168.8.11-csr.json | \
-cfssljson -bare 192.168.8.11
+cfssljson -bare kubelet-192.168.8.11
