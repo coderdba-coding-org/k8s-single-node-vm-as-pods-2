62603e13ae500ae85d6b22440f550009dbef83ed
coderdba <coderdba@coder.com>
2019-09-12 17:32:11 +0530

    updates

--- certs-ip-and-host-based/kubeconfig-kube-proxy-gen.sh
+++ certs-ip-and-host-based/kubeconfig-kube-proxy-gen.sh
@@ -2,17 +2,17 @@
 --certificate-authority=ca.pem \
 --embed-certs=true \
 --server=https://192.168.8.11:6443 \
---kubeconfig=kube-proxy.kubeconfig
+--kubeconfig=kubeconfig.kube-proxy
 
 kubectl config set-credentials kube-proxy \
 --client-certificate=kube-proxy.pem \
 --client-key=kube-proxy-key.pem \
 --embed-certs=true \
---kubeconfig=kube-proxy.kubeconfig
+--kubeconfig=kubeconfig.kube-proxy
 
 kubectl config set-context default \
 --cluster=kubernetes \
 --user=kube-proxy \
---kubeconfig=kube-proxy.kubeconfig
+--kubeconfig=kubeconfig.kube-proxy
 
-kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+kubectl config use-context default --kubeconfig=kubeconfig.kube-proxy
--- certs-ip-and-host-based/kubeconfig-kubelet-gen.sh
+++ certs-ip-and-host-based/kubeconfig-kubelet-gen.sh
@@ -1,10 +1,12 @@
+# Old kubeconfig file name
+#--kubeconfig=192.168.8.11.kubeconfig
+
 # Add the cluster information for 192.168.8.11
 kubectl config set-cluster kubernetes \
 --certificate-authority=ca.pem \
 --embed-certs=true \
 --server=https://192.168.8.11:6443 \
---kubeconfig=kubelet.kubeconfig
-#--kubeconfig=192.168.8.11.kubeconfig
+--kubeconfig=kubeconfig.kubelet.192.168.8.11
 
 # Add the credentials for 192.168.8.11.
 
@@ -12,18 +14,15 @@ kubectl config set-credentials system:node:192.168.8.11 \
 --client-certificate=192.168.8.11.pem \
 --client-key=192.168.8.11-key.pem \
 --embed-certs=true \
---kubeconfig=kubelet.kubeconfig
-#--kubeconfig=192.168.8.11.kubeconfig
+--kubeconfig=kubeconfig.kubelet.192.168.8.11
 
 # Add the context for 192.168.8.11.
 
 kubectl config set-context default \
 --cluster=kubernetes \
 --user=system:node:192.168.8.11 \
---kubeconfig=kubelet.kubeconfig
-#--kubeconfig=192.168.8.11.kubeconfig
+--kubeconfig=kubeconfig.kubelet.192.168.8.11
 
 # Use the context for 192.168.8.11.
 kubectl config use-context default \
---kubeconfig=kubelet.kubeconfig
-#--kubeconfig=192.168.8.11.kubeconfig
+--kubeconfig=kubeconfig.kubelet.192.168.8.11
